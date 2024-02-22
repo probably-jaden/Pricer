44882095397d5df3cc39e04b3b4f6a6e075beb55
Jaden Earl <jadendwightearl@gmail.com>
2024-02-22 00:05:00 -0700

    returns plot in demandPlot

--- R/MonopolyProfitabilityLibrary.R
+++ R/MonopolyProfitabilityLibrary.R
@@ -39,6 +39,11 @@ check_packages <- function() {
 #if(testBool) dp <- dplyr::tibble(wtp = c(64, 18, 46, 92, 110, 138, 113, 89, 0, 258, 205, 0, 18, 202, 46, 258, 0, 141, 0, 46, 61, 101, 64, 215, 95, 43, 46, 46, 132, 21, 18, 113, 9, 18, 21, 18, 104, 6, 0, 101, 6, 224, 322, 18, 316, 156, 104, 322, 285, 208, 316, 0, 288, 95, 6, 52, 46, 0, 18, 64, 98, 248, 18, 110, 0, 67, 0, 18, 0, 89, 132, 101, 18, 215, 18, 0, 0, 104, 285, 3, 46, 141, 322, 291, 89, 0, 101, 113, 67, 3, 132, 215, 224, 291, 9, 291, 267, 6, 6, 61, 178, 285, 64, 126, 0, 101, 15))
 
 
+#library(gridExtra)
+#library(scales)
+#library(data.table)
+#library(tidyverse)
+#dp <- demandDurable(dp)
 
 
 
@@ -523,9 +528,7 @@ demandPlot <- function(data, type, population, sample = NA){
           axis.title.x =element_text(size = 8),
           axis.title.y =element_text(size = 8))+
     theme(plot.title = element_text(face = "bold"))
-
-  suppressWarnings(print(newPlot))
-  return()
+  return(newPlot)
 }
 # if(testBool) demandPlot(tb, "Sigmoid", 1e6, 100)
 
@@ -953,6 +956,7 @@ demandCompare <- function(data, population, sample = NA, n = 3) {
   final_plot <-  suppressWarnings(do.call(grid.arrange, c(plot_list, ncol = 2)))
   return(final_plot)
 }
+
 # if(testBool) demandCompare(tb, 1e6)
 
 revenueCompare <- function(data, population, sample = NA, n = 3) {
@@ -971,6 +975,7 @@ revenueCompare <- function(data, population, sample = NA, n = 3) {
 }
 #Test
 # if(testBool) revenueCompare(tb, 1e6, 10)
+
 
 
 v <- 20
